2260e069850a0686dca6ddb9cc11c24da14d1488
Kevin Oganyi <kevoganyi@gmail.com>
2024-07-29 23:07:36 +0100

    sigh

--- app.js
+++ app.js
@@ -2,7 +2,25 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 const app = express()
+import rateLimit from "express-rate-limit"
+import helmet from 'helmet'
+import mongoSanitize from 'express-mongo-sanitize'
+import xss from 'xss-clean'
+import hpp from 'hpp'
+import cors from 'cors'
 
+// Rate limiting
+
+
+
+
+
+
+
+
+
+
+//routes
 
 
 export default app
